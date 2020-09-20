98b7cc938cfedc52950d27e1abe30eb5171a4a71
DongGeun <ydk1104@naver.com>
2020-09-20 20:35:15 +0900

    Fix 03

--- project1/03.sql
+++ project1/03.sql
@@ -1,1 +1,3 @@
-SELECT AVG(level) from CatchedPokemon where owner_id in (SELECT id from Trainer where hometown = 'Sangnok City')
+SELECT AVG(level) from CatchedPokemon where
+owner_id in (SELECT id from Trainer where hometown = 'Sangnok City')
+and pid in (SELECT id from Pokemon where type = 'Electric')
