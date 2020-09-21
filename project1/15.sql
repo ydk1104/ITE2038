07c5c1bddaa72d95c8d23df8a5393019448b23fa
DongGeun <ydk1104@naver.com>
2020-09-22 00:51:00 +0900

    Solve 15

---
+++ project1/15.sql
@@ -0,0 +1,5 @@
+SELECT owner_id, COUNT(owner_id) From CatchedPokemon
+GROUP BY owner_id
+Having COUNT(owner_id) = 
+(SELECT MAX(cnt) From (SELECT COUNT(owner_id) AS cnt From CatchedPokemon
+GROUP BY owner_id) AS T)
