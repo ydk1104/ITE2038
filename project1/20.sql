0ce1901fa4b0534ad883b619cbec9fa296d8452a
DongGeun <ydk1104@naver.com>
2020-09-22 01:03:26 +0900

    Solve 20

---
+++ project1/20.sql
@@ -0,0 +1,6 @@
+SELECT name, COUNT(*) as cnt From CatchedPokemon, Trainer Where
+CatchedPokemon.owner_id = Trainer.id and
+owner_id in
+(SELECT id From Trainer Where hometown = 'Sangnok City')
+GROUP BY owner_id
+ORDER BY cnt
