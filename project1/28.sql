4f012ce13fed8d7fe73adc61ad9b4da8b752ee9f
DongGeun <ydk1104@naver.com>
2020-09-22 22:36:45 +0900

    Solve 28

---
+++ project1/28.sql
@@ -0,0 +1,6 @@
+SELECT name, avg_level From
+(SELECT owner_id, AVG(level) as avg_level From CatchedPokemon Where pid in
+(SELECT id From Pokemon Where type = 'Normal' or type = 'Electric')
+GROUP BY owner_id) As temp, Trainer
+Where owner_id = Trainer.id
+ORDER BY avg_level
