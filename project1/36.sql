155031d65e452051ac8d61af62b2a706a0d08a1f
DongGeun <ydk1104@naver.com>
2020-09-23 01:59:19 +0900

    Solve 36

---
+++ project1/36.sql
@@ -0,0 +1,6 @@
+SELECT name From Trainer,
+(SELECT owner_id, nickname From CatchedPokemon Where pid in
+(SELECT after_id From Evolution Where after_id not in
+(SELECT before_id From Evolution))) As Temp
+Where Trainer.id = Temp.owner_id
+ORDER BY name
