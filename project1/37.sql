ed448792d49f9fa596b72c059808b2da1e269b15
DongGeun <ydk1104@naver.com>
2020-09-23 02:06:21 +0900

    Solve 37

---
+++ project1/37.sql
@@ -0,0 +1,7 @@
+SELECT name, S From
+(SELECT SUM(level) as S, owner_id From CatchedPokemon
+GROUP BY owner_id
+Having S =
+(SELECT MAX(S) From (SELECT SUM(level) as S From CatchedPokemon
+GROUP BY owner_id) as T)) as T, Trainer
+Where owner_id = id
