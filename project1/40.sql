47d78dee73738c289a09e262f23dba3a9a4b1c62
DongGeun <ydk1104@naver.com>
2020-09-23 03:14:38 +0900

    Solve 40

---
+++ project1/40.sql
@@ -0,0 +1,8 @@
+SELECT hometown, nickname From(
+SELECT hometown, MAX(max_level) as max_level_town From(
+SELECT owner_id, MAX(level) as max_level From CatchedPokemon
+GROUP BY owner_id) As Temp RIGHT JOIN Trainer
+ON owner_id = id
+GROUP BY hometown) As Temp LEFT JOIN CatchedPokemon
+ON max_level_town = level and hometown = (SELECT hometown From Trainer Where id=owner_id)
+ORDER BY hometown
