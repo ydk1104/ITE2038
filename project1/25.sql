3cb49f25aa898f0cae841af1d0617a17287be299
DongGeun <ydk1104@naver.com>
2020-09-22 22:20:29 +0900

    Solve 25

---
+++ project1/25.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT name From Pokemon where id in
+(SELECT pid From CatchedPokemon,Trainer where owner_id = Trainer.id && Trainer.hometown = 'Sangnok City') and id in
+(SELECT pid From CatchedPokemon,Trainer where owner_id = Trainer.id && Trainer.hometown = 'Brown City')
+ORDER BY name
