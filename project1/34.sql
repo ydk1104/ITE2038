963aa214618186f3167c6fd1eabb2af3945e55b5
DongGeun <ydk1104@naver.com>
2020-09-23 01:42:47 +0900

    Solve 34

---
+++ project1/34.sql
@@ -0,0 +1,4 @@
+SELECT name, level, nickname From
+(SELECT pid, level, nickname From CatchedPokemon Where owner_id in (SELECT leader_id From Gym) and nickname Like 'A%') As temp, Pokemon
+Where pid = id
+ORDER BY nickname DESC
