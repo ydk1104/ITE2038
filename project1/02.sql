3c415c28acac14cd415caf2a605a0a1cffabba96
DongGeun <ydk1104@naver.com>
2020-09-23 01:20:46 +0900

    Fix 02

--- project1/02.sql
+++ project1/02.sql
@@ -1,6 +1,6 @@
 SELECT name from Pokemon where type in
 (SELECT type from Pokemon GROUP BY type HAVING COUNT(type)>=
-(SELECT COUNT(type) from Pokemon
+(SELECT DISTINCT COUNT(type) from Pokemon
 GROUP BY type
 ORDER BY COUNT(type) DESC LIMIT 1,1))
 ORDER BY name
