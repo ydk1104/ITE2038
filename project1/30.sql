5ae7c0cf46d2214a9a15cecf4344d047154afe82
DongGeun <ydk1104@naver.com>
2020-09-23 14:44:51 +0900

    Fix 30-Don't print 4 or more evolve

--- project1/30.sql
+++ project1/30.sql
@@ -1,7 +1,9 @@
-SELECT X.before_id as first_id, X.before_name as first_name, X.after_name as second_name, Y.after_name as third_name from 
+SELECT X.before_id as first_id, X.before_name as first_name, X.after_name as second_name, Y.after_name as third_name from
 (SELECT T.before_id, T.after_id, T.before_name, name as after_name From
 (SELECT before_id, after_id, name as before_name From Evolution, Pokemon Where before_id = Pokemon.id) as T, Pokemon Where after_id =Pokemon.id) as X,
 (SELECT T.before_id, T.after_id, T.before_name, name as after_name From
 (SELECT before_id, after_id, name as before_name From Evolution, Pokemon Where before_id = Pokemon.id) as T, Pokemon Where after_id =Pokemon.id) as Y
 Where X.after_id = Y.before_id
+and X.before_id not in (SELECT after_id From Evolution)
+and Y.after_id not in (SELECT before_id From Evolution)
 ORDER BY first_id
