9a24c97e204c1575aa1fd14e844460bd4fd4a8cb
DongGeun <ydk1104@naver.com>
2020-09-23 19:05:02 +0900

    Fix 38, Print All Final Evolve(Q55)

--- project1/38.sql
+++ project1/38.sql
@@ -1,4 +1,4 @@
-SELECT name From Pokemon where id in
+SELECT name From Pokemon,
 (SELECT after_id From Evolution Where after_id not in
-(SELECT before_id From Evolution))
- ORDER BY name
+(SELECT before_id From Evolution)) As Temp Where Pokemon.id = Temp.after_id
+ORDER BY name
