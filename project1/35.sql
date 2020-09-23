2cb971552dbcc63dc769871de5e39352b800507b
DongGeun <ydk1104@naver.com>
2020-09-24 00:58:27 +0900

    Fix 35-Print Dupl

--- project1/35.sql
+++ project1/35.sql
@@ -1,3 +1,2 @@
-SELECT name From Pokemon Where id in
-(SELECT before_id From Evolution Where before_id > after_id)
+SELECT name From Pokemon, (SELECT before_id From Evolution Where before_id > after_id) As T Where id = before_id
 ORDER BY name
